b9e5dabb587a2174c38009132231ff43eb8816d3
casperOne <casperOne@caspershouse.com>
2016-08-27 22:44:41 -0400

    Updated build script.

--- build.ps1
+++ build.ps1
@@ -61,4 +61,4 @@ $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 ### exec { & dotnet test .\Ofl.Core\Ofl.Core -c Release }
 
-exec { & dotnet pack .\Ofl.Core\Ofl.Core -c Release -o .\artifacts --version-suffix=$revision }  
+exec { & dotnet pack .\Ofl.Core -c Release -o .\artifacts --version-suffix=$revision }  
