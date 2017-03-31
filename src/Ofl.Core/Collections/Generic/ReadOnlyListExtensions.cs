98629debec46a8faa5a242bbaf9a64ad907d2e77
casperOne <casperOne@caspershouse.com>
2017-03-31 09:45:10 -0400

    Added overloads for BinarySearch which take a map from object -> property.

--- src/Ofl.Core/Collections/Generic/ReadOnlyListExtensions.cs
+++ src/Ofl.Core/Collections/Generic/ReadOnlyListExtensions.cs
@@ -8,25 +8,54 @@ namespace Ofl.Core.Collections.Generic
         public static int BinarySearch<T>(this IReadOnlyList<T> list, T value)
         {
             // Call the overload.
-            return list.BinarySearch(value, Comparer<T>.Default);
+            return list.BinarySearch(t => t, value);
+        }
+
+        public static int BinarySearch<TSource, TValue>(this IReadOnlyList<TSource> list, Func<TSource, TValue> map, TValue value)
+        {
+            // Call the overload.
+            return list.BinarySearch(map, value, Comparer<TValue>.Default);
         }
 
         public static int BinarySearch<T>(this IReadOnlyList<T> list, int index, int length, T value)
         {
             // Call the overload.
-            return list.BinarySearch(index, length, value, Comparer<T>.Default);
+            return list.BinarySearch(index, length, t => t, value);
         }
+
+        public static int BinarySearch<TSource, TValue>(this IReadOnlyList<TSource> list, int index, int length, Func<TSource, TValue> map, TValue value)
+        {
+            // Call the overload.
+            return list.BinarySearch(index, length, map, value, Comparer<TValue>.Default);
+        }
+
 
         public static int BinarySearch<T>(this IReadOnlyList<T> list, T value, IComparer<T> comparer)
         {
             // Call the overload.
-            return list.BinarySearch(0, list.Count, value, comparer);
+            return list.BinarySearch(0, list.Count, t => t, value, comparer);
         }
+
+        public static int BinarySearch<TSource, TValue>(this IReadOnlyList<TSource> list, Func<TSource, TValue> map, TValue value, IComparer<TValue> comparer)
+        {
+            // Call the overload.
+            return list.BinarySearch(0, list.Count, map, value, comparer);
+        }
+
 
         public static int BinarySearch<T>(this IReadOnlyList<T> list, int index, int length, T value, IComparer<T> comparer)
         {
+            // Call the overload.
+            return list.BinarySearch(index, length, t => t, value, comparer);
+        }
+
+        public static int BinarySearch<TSource, TValue>(this IReadOnlyList<TSource> list, int index, int length, Func<TSource, TValue> map, TValue value,
+            IComparer<TValue> comparer)
+        {
             // Validate parameters.
             if (list == null) throw new ArgumentNullException(nameof(list));
+            if (map == null) throw new ArgumentNullException(nameof(map));
+            if (comparer == null) throw new ArgumentNullException(nameof(comparer));
             if (index < 0) throw new ArgumentOutOfRangeException(nameof(index), index, $"The { nameof(index) } parameter must be a non-negative value.");
             if (length < 0) throw new ArgumentOutOfRangeException(nameof(length), length, $"The {nameof(length)} parmeter must be a non-negative value.");
             if (index + length > list.Count) throw new InvalidOperationException($"The value of {nameof(index)} plus {nameof(length)} must be less than or equal to the value of the number of items in the {nameof(list)}.");
@@ -44,7 +73,7 @@ namespace Ofl.Core.Collections.Generic
                 int midpoint = low + ((high - low) >> 1);
 
                 // Compare.
-                int order = comparer.Compare(list[midpoint], value);
+                int order = comparer.Compare(map(list[midpoint]), value);
 
                 // If they are equal, return.
                 if (order == 0) return midpoint;
