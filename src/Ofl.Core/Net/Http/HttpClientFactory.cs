04db993281de6b2accc201b749c5f064c30f2152
casperOne <casperOne@caspershouse.com>
2017-05-12 11:23:40 -0400

    Added ValueTaskExtensions.

--- src/Ofl.Core/Net/Http/HttpClientFactory.cs
+++ src/Ofl.Core/Net/Http/HttpClientFactory.cs
@@ -2,18 +2,10 @@
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Ofl.Core.Threading.Tasks;
 
 namespace Ofl.Core.Net.Http
 {
-    //////////////////////////////////////////////////
-    ///
-    /// <author>Nicholas Paldino</author>
-    /// <created>2012-08-04</created>
-    /// <summary>The implementation of the
-    /// factory for generating <see cref="HttpClient"/>
-    /// instances.</summary>
-    ///
-    //////////////////////////////////////////////////
     public class HttpClientFactory : IHttpClientFactory
     {
         public Task<HttpClient> CreateAsync(HttpMessageHandler handler, bool disposeHandler, CancellationToken cancellationToken)
