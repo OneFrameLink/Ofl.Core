52c1134c703fc20c15ed9b679d7b9e1d262489f8
casperOne <casperOne@caspershouse.com>
2017-05-17 16:12:55 -0400

    Intermediate commit.

---
+++ src/Ofl.Serialization.Json.Newtonsoft/JsonSerializerSettingsExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Ofl.Serialization.Json.Newtonsoft
+{
+    public static class JsonSerializerSettingsExtensions
+    {
+        public static JsonSerializer ToSerializer(this JsonSerializerSettings jsonSerializerSettings)
+        {
+            // Validate parameters.
+            if (jsonSerializerSettings == null) throw new ArgumentNullException(nameof(jsonSerializerSettings));
+
+            // Create the serializer.
+            return JsonSerializer.Create(jsonSerializerSettings);
+        }
+
+        public static Task<MemoryStream> SerializeToMemoryStreamAsync<TRequest>(this JsonSerializerSettings jsonSerializerSettings, TRequest request,
+            CancellationToken cancellationToken) => jsonSerializerSettings.ToSerializer().SerializeToMemoryStreamAsync(request, cancellationToken);
+
+        public static string SerializeToString<TRequest>(this JsonSerializerSettings jsonSerializerSettings, TRequest request) =>
+            jsonSerializerSettings.ToSerializer().SerializeToString(request);
+    }
+}
